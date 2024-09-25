49950f9d59de19e826e741443caa62a625c22f73
DarkoCBS <darkocbs9@gmail.com>
2024-09-25 09:22:58 +0200

    demo

--- src/components/ShipList.tsx
+++ src/components/ShipList.tsx
@@ -18,137 +18,137 @@ export default function ShipList() {
   const [masterKeyEncrypted, setMasterKeyEncrypted] = useState<number[] | null>(
     null
   );
-  // const [shipAccounts, setShipAccounts] = useState<ProgramAccount<{ ship: PublicKey; shipManagement: PublicKey; dataAccounts: PublicKey[]; }>[] | null>(null);
-  // const [hasAccess, setHasAccess] = useState<boolean[]>([]);
-  // const [unapprovedExternalObservers, setUnapprovedExternalObservers] = useState<boolean[]>([]);
+  const [shipAccounts, setShipAccounts] = useState<ProgramAccount<{ ship: PublicKey; shipManagement: PublicKey; dataAccounts: PublicKey[]; }>[] | null>(null);
+  const [hasAccess, setHasAccess] = useState<boolean[]>([]);
+  const [unapprovedExternalObservers, setUnapprovedExternalObservers] = useState<boolean[]>([]);
 
   // MOCK DATA
-  const [shipAccounts, setShipAccounts] = useState([
-    {
-      publicKey: "mockPublicKey1",
-      account: {
-        ship: "mockShip1",
-        shipManagement: "mockShipManagement1",
-        dataAccounts: ["mockDataAccount1"],
-      },
-    },
-    {
-      publicKey: "mockPublicKey2",
-      account: {
-        ship: "mockShip2",
-        shipManagement: "mockShipManagement2",
-        dataAccounts: ["mockDataAccount1"],
-      },
-    },
-  ]);
-  const [hasAccess, setHasAccess] = useState([true, true]);
-  const [unapprovedExternalObservers, setUnapprovedExternalObservers] =
-    useState([false, false]);
+//   const [shipAccounts, setShipAccounts] = useState([
+//     {
+//       publicKey: "mockPublicKey1",
+//       account: {
+//         ship: "mockShip1",
+//         shipManagement: "mockShipManagement1",
+//         dataAccounts: ["mockDataAccount1"],
+//       },
+//     },
+//     {
+//       publicKey: "mockPublicKey2",
+//       account: {
+//         ship: "mockShip2",
+//         shipManagement: "mockShipManagement2",
+//         dataAccounts: ["mockDataAccount1"],
+//       },
+//     },
+//   ]);
+//   const [hasAccess, setHasAccess] = useState([true, true]);
+//   const [unapprovedExternalObservers, setUnapprovedExternalObservers] =
+//     useState([false, false]);
 
-  useEffect(() => {
-    const fetchAllShipAccounts = async () => {
-      try {
-        if (!publicKey) {
-          console.error("Wallet not connected");
-          return;
-        }
+const fetchAllShipAccounts = async () => {
+    try {
+      if (!publicKey) {
+        console.error("Wallet not connected");
+        return;
+      }
 
-        // Fetch initial account data
-        let shipAccounts = await program.account.shipAccount.all();
-        console.log("Ship Accounts:", JSON.stringify(shipAccounts, null, 2));
+      // Fetch initial account data
+      let shipAccounts = await program.account.shipAccount.all();
+      console.log("Ship Accounts:", JSON.stringify(shipAccounts, null, 2));
 
-        // TODO:
-        shipAccounts = shipAccounts.filter(
-          (shipAccount) => shipAccount.account.dataAccounts.length > 0
-        );
+      // TODO:
+      shipAccounts = shipAccounts.filter(
+        (shipAccount) => shipAccount.account.dataAccounts.length > 0
+      );
 
-        setShipAccounts(shipAccounts);
+      setShipAccounts(shipAccounts);
 
-        const accessArray = new Array(shipAccounts.length).fill(false);
-        const unapprovedArray = new Array(shipAccounts.length).fill(false);
+      const accessArray = new Array(shipAccounts.length).fill(false);
+      const unapprovedArray = new Array(shipAccounts.length).fill(false);
 
-        await Promise.all(
-          shipAccounts.map(async (shipAccount, shipIndex) => {
-            if (shipAccount.account.dataAccounts.length === 0) {
-              accessArray[shipIndex] = false;
-              unapprovedArray[shipIndex] = false;
-            }
+      await Promise.all(
+        shipAccounts.map(async (shipAccount, shipIndex) => {
+          if (shipAccount.account.dataAccounts.length === 0) {
+            accessArray[shipIndex] = false;
+            unapprovedArray[shipIndex] = false;
+          }
 
-            await Promise.all(
-              shipAccount.account.dataAccounts.map(async (dataAccount) => {
-                const [externalObserversAccountAddr, _] =
-                  PublicKey.findProgramAddressSync(
-                    [
-                      Buffer.from("external_observers_account"),
-                      dataAccount.toBuffer(),
-                    ],
-                    program.programId
-                  );
-
-                const externalObserversAccountData =
-                  await program.account.externalObserversAccount.fetch(
-                    externalObserversAccountAddr
-                  );
-
-                console.log(
-                  "External Observers Account Data:",
-                  JSON.stringify(externalObserversAccountData, null, 2)
+          await Promise.all(
+            shipAccount.account.dataAccounts.map(async (dataAccount) => {
+              const [externalObserversAccountAddr, _] =
+                PublicKey.findProgramAddressSync(
+                  [
+                    Buffer.from("external_observers_account"),
+                    dataAccount.toBuffer(),
+                  ],
+                  program.programId
                 );
 
-                const hasAccess =
-                  externalObserversAccountData.externalObservers.some(
+              const externalObserversAccountData =
+                await program.account.externalObserversAccount.fetch(
+                  externalObserversAccountAddr
+                );
+
+              console.log(
+                "External Observers Account Data:",
+                JSON.stringify(externalObserversAccountData, null, 2)
+              );
+
+              const hasAccess =
+                externalObserversAccountData.externalObservers.some(
+                  (observer) => observer.equals(publicKey)
+                );
+              const isUnapproved =
+                externalObserversAccountData.unapprovedExternalObservers.some(
+                  (unapprovedObserver) => unapprovedObserver.equals(publicKey)
+                );
+
+              if (hasAccess) {
+                accessArray[shipIndex] = true;
+                const index =
+                  externalObserversAccountData.externalObservers.findIndex(
                     (observer) => observer.equals(publicKey)
                   );
-                const isUnapproved =
-                  externalObserversAccountData.unapprovedExternalObservers.some(
-                    (unapprovedObserver) => unapprovedObserver.equals(publicKey)
-                  );
+                const masterKeyEncrypted =
+                  externalObserversAccountData.externalObserversMasterKeys[
+                    index
+                  ];
+                setMasterKeyEncrypted(masterKeyEncrypted);
+              }
 
-                if (hasAccess) {
-                  accessArray[shipIndex] = true;
-                  const index =
-                    externalObserversAccountData.externalObservers.findIndex(
-                      (observer) => observer.equals(publicKey)
-                    );
-                  const masterKeyEncrypted =
-                    externalObserversAccountData.externalObserversMasterKeys[
-                      index
-                    ];
-                  setMasterKeyEncrypted(masterKeyEncrypted);
-                }
+              if (isUnapproved) {
+                unapprovedArray[shipIndex] = true;
+              }
 
-                if (isUnapproved) {
-                  unapprovedArray[shipIndex] = true;
-                }
+              console.log("Has Access:", accessArray);
+              console.log("Unapproved External Observers:", unapprovedArray);
+            })
+          );
+        })
+      );
 
-                console.log("Has Access:", accessArray);
-                console.log("Unapproved External Observers:", unapprovedArray);
-              })
-            );
-          })
-        );
+      setHasAccess([...accessArray]);
+      setUnapprovedExternalObservers([...unapprovedArray]);
+    } catch (error) {
+      console.error("Error fetching counter data:", error);
+    }
+  };
 
-        setHasAccess([...accessArray]);
-        setUnapprovedExternalObservers([...unapprovedArray]);
-      } catch (error) {
-        console.error("Error fetching counter data:", error);
-      }
-    };
-
-    // fetchAllShipAccounts();
+  useEffect(() => {
+    fetchAllShipAccounts();
   }, [program, connection, publicKey]);
 
   const handleRequestAccess = async (index: number) => {
     console.log(`Requesting access for ship account at index ${index}`);
 
-    // MOCK IMPLEMENTATION
-    navigate("/view-data", {
-      state: {
-        ship: shipAccounts[index].account.ship,
-        masterKeyDecrypted: new Uint8Array([1, 2, 3, 4]),
-      },
-    });
-    return;
+    // // MOCK IMPLEMENTATION
+    // navigate("/view-data", {
+    //   state: {
+    //     ship: shipAccounts[index].account.ship,
+    //     masterKeyDecrypted: new Uint8Array([1, 2, 3, 4]),
+    //   },
+    // });
+    // return;
 
     const message = "SIGN THIS MESSAGE TO GET SEED FOR25199 KEYPAIR";
     const encodedMessage = new TextEncoder().encode(message);
@@ -199,6 +199,7 @@ export default function ShipList() {
       });
 
       console.log("Transaction sent with signature: ", txSignature);
+      fetchAllShipAccounts();
     } catch (error) {
       console.error("Error signing message:", error);
       alert("Failed to sign message.");
@@ -206,11 +207,11 @@ export default function ShipList() {
   };
 
   const handleViewData = async (ship: string) => {
-    // MOCK IMPLEMENTATION
-    navigate("/view-data", {
-      state: { ship, masterKeyDecrypted: new Uint8Array([1, 2, 3, 4]) },
-    });
-    return;
+    // // MOCK IMPLEMENTATION
+    // navigate("/view-data", {
+    //   state: { ship, masterKeyDecrypted: new Uint8Array([1, 2, 3, 4]) },
+    // });
+    // return;
 
     const message = "SIGN THIS MESSAGE TO GET SEED FOR25199 KEYPAIR";
     const encodedMessage = new TextEncoder().encode(message);
@@ -234,10 +235,14 @@ export default function ShipList() {
           masterKeyEncryptedUint8Array
         );
         console.log("skUint8Array:", new Uint8Array(Buffer.from(sk, "hex")));
-        masterKeyDecrypted = ecies25519.decryptSync(
+
+        console.log("TEST: ", await ecies25519.encrypt(new Uint8Array(32), x25519.getPublicKey(sk)))
+        masterKeyDecrypted = await ecies25519.decrypt(
           masterKeyEncryptedUint8Array,
           new Uint8Array(Buffer.from(sk, "hex"))
         );
+
+        console.log("masterKeyDecrypted: ", masterKeyDecrypted);
       } else {
         console.error("masterKeyEncrypted is null");
       }
