dfe148ea59cf1f513572ba9e43d16e63cbecf7c5
RastkoCBS <rastko@ceresblockchain.solutions>
2024-11-20 13:26:10 +0100

    chore: Update ShipMap component with navigation button

--- src/components/ShipMap.tsx
+++ src/components/ShipMap.tsx
@@ -4,8 +4,12 @@ import "../styles/ShipMap.css";
 import crypt from "crypto-browserify";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import L from "leaflet";
+import { useNavigate } from "react-router-dom";
+
+
 
 export default function ShipMap() {
+	const navigate = useNavigate();
 	const positions = [
 		{ id: 1, name: "Ship Alpha", lat: 60, lng: 30 },
 		{ id: 2, name: "Ship Beta", lat: 40, lng: -20 },
@@ -22,14 +26,13 @@ export default function ShipMap() {
 			<div className="map-container">
         <MapContainer 
 					style={ {width: "100%", height: "100%"}} 
-					center={[0, 0]} 
+					center={[20, 0]} 
 					zoom={3} 
-					minZoom={3} 
+					minZoom={2} 
 					maxZoom={10} 
 					scrollWheelZoom={true} 
-					// worldCopyJump={true}
+					worldCopyJump={true}
 					maxBounds={bounds}
-        	maxBoundsViscosity={1.0}
 					>
 					<TileLayer
 						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -41,6 +44,20 @@ export default function ShipMap() {
 							<strong>{position.name}</strong>
 							<br />
 							Coordinates: {position.lat}, {position.lng}
+							<br />
+							<button
+                onClick={() => navigate(`/ship-info/`)}
+                style={{
+                  padding: "5px 10px",
+                  backgroundColor: "#6dbddc",
+                  color: "#fff",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+              >
+                View Info
+              </button>
 						</Popup>
 					</Marker>
 					))}
